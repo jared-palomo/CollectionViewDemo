f1d49a7154336bef96e4d6472ae4870333a3e15a
Jared Palomo <g.jared.palomo@gmail.com>
2022-03-08 16:40:47 -0600

    Agregar archivos de proyecto.

---
+++ CollectionViewDemo/CollectionViewDemo/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CollectionViewDemo.Models;
+using CollectionViewDemo.ViewModels;
+using CollectionViewDemo.Views;
+using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViewDemo
+{
+    public partial class App : Application
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CollectionViewDemo/CollectionViewDemo/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CollectionViewDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
